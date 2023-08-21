bdb58308528605bfb9387a72a2d9aca1410d03c6
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-08-21 03:26:41 -0300

    Nest 101 - Input Validation - 21/08/2023
    Class Validator Decorators and Pipes

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,24 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateArticleDto {
+    @IsString()
+    @MinLength(5)
     @ApiProperty()
     title: string;
 
-    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    @MaxLength(300)
+    @ApiProperty({ required: false })
     description?: string;
 
+    @IsString()
     @ApiProperty()
     body: string;
 
-    @ApiProperty()
+    @IsBoolean()
+    @IsOptional()
+    @ApiProperty({ required: false })
     published?: boolean = false;
 }
